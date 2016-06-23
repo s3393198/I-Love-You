3e91572b7abd0d3fa4b4c4b5792c6f816f975da6
Nắng Trong Vườn <morepositive@outlook.com>
2016-06-23 14:08:39 +0700

    added game loop
    added the main game loop

--- src/iloveyou/Iloveyou.java
+++ src/iloveyou/Iloveyou.java
@@ -5,11 +5,19 @@
  */
 package iloveyou;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
@@ -19,11 +27,44 @@ public class Iloveyou extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+       
+        stage.setTitle("Project I Love You");
         
+        Group root = new Group();
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
+        
+        Canvas canvas = new Canvas(1024, 768);
+        root.getChildren().add(canvas);
+        
+        Timeline gameLoop = new Timeline();
+        gameLoop.setCycleCount(Animation.INDEFINITE);
+        
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        
+        Image test = new Image("assets/earth.png");
+        
+        final long timeStart = System.currentTimeMillis();
+        
+        KeyFrame kf = new KeyFrame(Duration.seconds(0.017), new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent ae) {
+                
+                double t = (System.currentTimeMillis() - timeStart) / 1000.0;
+               
+                double x = Math.cos(t);
+                double y = Math.sin(t);
+                
+                gc.clearRect(0, 0, 768, 1024);
+                
+                gc.drawImage(test, x,y);
+               
+            }
+        });
+        
+        gameLoop.getKeyFrames().add(kf);
+        gameLoop.play();
+        
+        stage.setResizable(false); 
         stage.show();
     }
 
